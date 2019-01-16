2f9308c02504a3f2c2f4778360fec444166a0273
ennovatenow <anish.thomas@evolvussolutions.com>
2019-01-16 05:50:21 +0530

    added menu scripts

--- create_migration_script.sh
+++ create_migration_script.sh
@@ -3,9 +3,38 @@ MIGRATION=$1
 MIGRATION_SCRIPT="$MIGRATION.sh"
 if [ -f $MIGRATION_FOLDER/$MIGRATION.txt ]; then
    filelines=`cat $MIGRATION_FOLDER/$MIGRATION.txt`
+   echo "#!/bin/bash" >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'clear' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo "***************************************************"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo "*                                                 *"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo "*          Precision 100 Framework                *"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo "*                                                 *"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'echo "***************************************************"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo "echo" >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo "PS3='Please enter your choice: (3 to quit) '" >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   menus=`cat $MIGRATION_FOLDER/$MIGRATION.txt | tr '\n' ' '`
+   echo "options=($menus quit)" >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'select opt in "${options[@]}"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo "do" >>  $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+   echo 'case $opt in' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+
    for pipeline in $filelines ; do
+
        $PRECISION100_FOLDER/create_pipeline_script.sh $pipeline
+
+       echo "\"$pipeline\")" >>  $MIGRATION_FOLDER/$MIGRATION_SCRIPT
        echo "$PIPELINE_FOLDER/$pipeline".sh >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+       #echo 'echo "you chose choice 1"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+       echo '    ;;' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+
    done;
+
+   echo '"quit")' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
+   echo '   break ' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
+   echo '   ;; ' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
+   echo ' *) echo invalid option;;' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
+   echo ' esac' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
+   echo 'done' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT 
    chmod u+x $MIGRATION_FOLDER/$MIGRATION_SCRIPT
 fi
--- create_pipeline_script.sh
+++ create_pipeline_script.sh
@@ -4,9 +4,37 @@ PIPELINE=$1
 PIPELINE_SCRIPT="$PIPELINE".sh
 if [ -f $PIPELINE_FOLDER/$PIPELINE.txt ]; then
    filelines=`cat $PIPELINE_FOLDER/$PIPELINE.txt`
+   echo "#!/bin/bash" >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'clear' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo "***************************************************"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo "*                                                 *"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo "*          Precision 100 Framework                *"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo "*                                                 *"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'echo "***************************************************"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo "echo" >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo "PS3='Please enter your choice: (3 to quit) '" >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   menus=`cat $PIPELINE_FOLDER/$PIPELINE.txt | tr '\n' ' '`
+   echo "options=($menus quit)" >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'select opt in "${options[@]}"' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo "do" >>  $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+   echo 'case $opt in' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+
    for container in $filelines ; do
+
        $PRECISION100_FOLDER/create_container_script.sh $container
+
+       echo "\"$container\")" >>  $PIPELINE_FOLDER/$PIPELINE_SCRIPT
        echo "$CONTAINER_FOLDER/$container/container.sh" >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
+       #echo 'echo "you chose choice 1"' >> $MIGRATION_FOLDER/$MIGRATION_SCRIPT
+       echo '    ;;' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT
    done;
+
+   echo '"quit")' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
+   echo '   break ' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
+   echo '   ;; ' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
+   echo ' *) echo invalid option;;' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
+   echo ' esac' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
+   echo 'done' >> $PIPELINE_FOLDER/$PIPELINE_SCRIPT 
    chmod u+x $PIPELINE_FOLDER/$PIPELINE_SCRIPT
 fi
