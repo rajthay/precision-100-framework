23b767f5e4b6febc6cac3be07d2e46d5127c92bd
ennovatenow <anish.thomas@evolvussolutions.com>
2019-02-04 12:01:06 +0530

    corrected folder structure to suit install

--- install/.oraenv.sh
+++
@@ -1,5 +0,0 @@
-export ORACLE_HOME=/usr/lib/oracle/18.3/client64/
-export LD_LIBRARY_PATH="$ORACLE_HOME"/lib
-export PATH="$ORACLE_HOME/bin:$PATH"
-export TNS_ADMIN="$ORACLE_HOME/lib/network/admin"
-
