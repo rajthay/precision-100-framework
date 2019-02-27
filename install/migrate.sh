bfe648e017774ad59b8df32b70e11de96d3a9f7f
ennovatenow <anish.thomas@evolvussolutions.com>
2019-02-27 21:38:47 +0530

    added migrate.sh

---
+++ install/migrate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ ( "$#" -gt 2 ) ]]; then
+  echo "Usage: $0 [simulation-mode-true-false] [simulation-sleep-time-in-seconds]"
+  exit 1;
+fi
+
+source ./conf/.env.sh
+if [ ! -z "$1" ]; then
+    export SIMULATION_MODE="TRUE"
+fi
+if [ ! -z "$2" ]; then
+    export SIMULATION_SLEEP=$3
+fi
+
+export OPERATION="migrate.sh"
+
+DATAFLOW_FILES=`basename --suffix .txt -a $DATAFLOW_FOLDER/*.txt`
+echo $DATAFLOW_FILES;
+clear
+echo "****************************************************************"
+echo "                                                                "
+echo "                  Precision 100 Execution                       "
+echo "                                                                "
+echo "  Project Name: $PROJECT_FOLDER                                 "
+echo "                                                                "
+echo "  Iteration: $EXECUTION_NAME                                    "
+echo "                                                                "
+echo "  Simulation Mode: $SIMULATION_MODE                             "
+echo "                                                                "
+echo "****************************************************************"
+select i in $DATAFLOW_FILES "Quit";
+do
+    case $i in 
+      "Quit")
+         break;
+      ;;
+      *)
+        $PRECISION100_FOLDER/bin/exec_dataflow.sh ${i%.*}
+        break;
+    esac;
+done;
