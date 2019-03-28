6df71960cc58f54e3c86124252d327b3573f4956
ennovatenow <anish.thomas@evolvussolutions.com>
2019-03-28 11:27:41 +0530

    fixed issue #4

--- install/operators/map-file/bin/map-file-o-table.sh
+++ install/operators/map-file/bin/map-file-o-table.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+REVERSE_TABLE_NAME_PREFIX=${DEFAULT_REVERSE_TABLE_NAME_PREFIX:-R}
 TABLE_NAME_PREFIX=${DEFAULT_TABLE_NAME_PREFIX:-O}
 FILE_NAME_SUFFIX=${DEFAULT_FILE_NAME_SUFFIX:-csv}
 COLUMN_DATA_TYPE=${DEFAULT_COLUMN_DATA_TYPE:-NVARCHAR2(2000)}
@@ -12,6 +13,26 @@ TABLE_NAME=$1
 SOURCE_FILE=$2
 echo "DROP TABLE ${TABLE_NAME_PREFIX}_${TABLE_NAME};"
 echo "CREATE TABLE ${TABLE_NAME_PREFIX}_${TABLE_NAME} ("
+counter=0
+while IFS=$MAP_FILE_DELIMITER read -r column_name data_type max_length mapping_code mapping_value;
+do
+  if [[ counter -eq 0 ]]; then
+    counter=$counter+1;
+    continue;
+  fi
+  if [[ counter -eq 1 ]]; then
+    counter=$counter+1;
+    echo "  $column_name $COLUMN_DATA_TYPE"
+    continue;
+  fi
+  counter=$counter+1;
+  echo ", $column_name $COLUMN_DATA_TYPE"
+done < <(cat ${SOURCE_FILE} | tr '\t' '~' | tr -d '\r')
+echo ");"
+
+# Create the r tables
+echo "DROP TABLE ${REVERSE_TABLE_NAME_PREFIX}_${TABLE_NAME};"
+echo "CREATE TABLE ${REVERSE_TABLE_NAME_PREFIX}_${TABLE_NAME} ("
 counter=0
 while IFS=$MAP_FILE_DELIMITER read -r column_name data_type max_length mapping_code mapping_value;
 do
