32d215332421883174023f87d7a32a7bacb5476a
ennovatenow <anish.thomas@evolvussolutions.com>
2019-03-30 15:31:52 +0530

    fix for issue #16

--- install/bin/get-files.sh
+++ install/bin/get-files.sh
@@ -7,6 +7,8 @@ if [[ -f $CONTAINER_FOLDER/$CONTAINER/container.reg ]]; then
 else
   if [[ -f $CONTAINER_FOLDER/$CONTAINER/file.txt ]]; then
      cat $CONTAINER_FOLDER/$CONTAINER/file.txt
+     echo "Template still uses file.txt - please move to container.reg as early as possible." 1>&2
+     echo "Templates using file.txt will not be supported in the future." 1>&2
   else
     echo "Registry file in $CONTAINER_FOLDER/$CONTAINER does not exist" 1>&2
   fi
